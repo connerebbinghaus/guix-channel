368c7211f8725ba0e0e5ace1caba9c2a2fd218c9
Conner Ebbinghaus <connerebbinghaus@gmail.com>
2024-08-26 16:33:48 -0400

    Fix prism-launcher

--- conner/packages/prism-launcher.scm
+++ conner/packages/prism-launcher.scm
@@ -4,6 +4,7 @@
   #:use-module (guix packages)
   #:use-module (guix git-download)
   #:use-module (guix build-system cmake)
+  #:use-module (gnu packages bash)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages qt)
   #:use-module (gnu packages markup)
@@ -13,7 +14,9 @@
   #:use-module (gnu packages gl)
   #:use-module (gnu packages vulkan)
   #:use-module (gnu packages java)
-  #:use-module (gnu packages xdisorg))
+  #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages xorg)
+  #:use-module (gnu packages pulseaudio))
 
 (define-public prism-launcher
   (package
@@ -38,22 +41,49 @@
 				      ;"libraries/tomlplusplus" ; FIXME: something is wrong with guix's tomlplusplus, strange cmake errors
 				      "libraries/zlib")))))
    (arguments
-    `(#:configure-flags '("-DLauncher_QT_VERSION_MAJOR=5"))) ; Use Qt5, guix's quazip uses it
+    `(#:configure-flags '("-DLauncher_QT_VERSION_MAJOR=5") ; Use Qt5, guix's quazip uses it
+      #:phases
+      (modify-phases %standard-phases ; Taken from https://gitlab.com/guix-gaming-channels/games/-/blob/66fe8b72d114ee2de218fc46d0f7ad95d8b3129e/games/packages/minecraft.scm
+		     (add-after 'install 'patch-paths
+				(lambda* (#:key inputs outputs #:allow-other-keys)
+				  (let* ((out            (assoc-ref outputs "out"))
+					 (bin            (string-append out "/bin/prismlauncher"))
+					 (xrandr         (assoc-ref inputs "xrandr"))
+					 (qtwayland      (assoc-ref inputs "qtwayland")))
+				    (wrap-program bin
+						  `("PATH" ":" prefix (,(string-append xrandr "/bin")))
+						  `("QT_PLUGIN_PATH" ":" prefix (,(string-append
+										   qtwayland "/lib/qt5/plugins")))
+						  `("LD_LIBRARY_PATH" ":" prefix
+						    (,@(map (lambda (dep)
+							      (string-append (assoc-ref inputs dep)
+									     "/lib"))
+							    '("libx11" "libxext" "libxcursor"
+							      "libxrandr" "libxxf86vm" "pulseaudio" "mesa")))))
+				    #t))))))
    (build-system cmake-build-system)
    (native-inputs
     (list extra-cmake-modules pkg-config))
    (inputs
     (list
+     bash-minimal
      qtbase-5
+     qtwayland
      cmark
      gulrak-filesystem
      quazip
      ;tomlplusplus ; FIXME
      zlib
      mesa
-     vulkan-headers
-     libxkbcommon
-     `(,openjdk17 "jdk")))
+     libx11
+     libxext
+     libxcursor
+     libxrandr
+     libxxf86vm
+     pulseaudio
+     mesa
+     xrandr))
+(propagated-inputs (list `(,openjdk17 "jdk")))
    (home-page "https://prismlauncher.org/")
    (synopsis "Custom launcher for Minecraft")
    (description "Prism Launcher is a custom launcher for Minecraft that allows you to easily manage multiple installations of Minecraft at once.")
