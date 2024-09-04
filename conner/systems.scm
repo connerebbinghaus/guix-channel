0f283cdec6db656ae4bcbfe3723f7cd133d95d97
Conner Ebbinghaus <connerebbinghaus@gmail.com>
2024-09-04 18:37:38 -0400

    Add conner-pc substitute key

--- conner/systems.scm
+++ conner/systems.scm
@@ -29,7 +29,9 @@
 	    %default-substitute-urls))
    (authorized-keys
     (append (list (plain-file "non-guix.pub"
-			      "(public-key (ecc (curve Ed25519) (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))"))
+			      "(public-key (ecc (curve Ed25519) (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))")
+		  (plain-file "conner-pc.pub"
+			      "(public-key (ecc (curve Ed25519) (q #69138244040AC8BBDF3241686EE3B5D9D070299E439F35B85D836322DBCA814B#)))"))
 	    %default-authorized-guix-keys))))
 
 (define-public common-extra-services
