5d2bf42d77469489bb4de5478850af85d3679226
Conner Ebbinghaus <connerebbinghaus@gmail.com>
2024-12-19 18:26:57 -0500

    Emacs configuration

--- conner/users/files/.config/emacs/init.el
+++ conner/users/files/.config/emacs/init.el
@@ -4,5 +4,10 @@
   (add-to-list 'geiser-guile-load-path "~/Projects/nonguix")
   (add-to-list 'geiser-guile-load-path "~/Projects/sops-guix")
   (add-to-list 'geiser-guile-load-path "~/Projects/guix-surface"))
-  
+
+(require 'lsp-mode)
+(add-hook 'prog-mode-hook #'lsp-deferred)
+
+(global-flycheck-mode +1)  
+(add-hook 'after-init-hook 'global-company-mode)
 (add-hook 'after-init-hook 'envrc-global-mode)
