1c50e922dcfd238625d580e876b8450fc4040010
Conner Ebbinghaus <connerebbinghaus@gmail.com>
2024-12-19 23:23:39 -0500

    Fix emacs geiser load path

--- conner/users/files/.config/emacs/init.el
+++ conner/users/files/.config/emacs/init.el
@@ -2,12 +2,12 @@
 (with-eval-after-load 'geiser-guile
   (add-to-list 'geiser-guile-load-path "~/Projects/guix")
   (add-to-list 'geiser-guile-load-path "~/Projects/nonguix")
-  (add-to-list 'geiser-guile-load-path "~/Projects/sops-guix")
+  (add-to-list 'geiser-guile-load-path "~/Projects/sops-guix/modules")
   (add-to-list 'geiser-guile-load-path "~/Projects/guix-surface"))
 
 (require 'lsp-mode)
 (add-hook 'prog-mode-hook #'lsp-deferred)
 
-(global-flycheck-mode +1)  
+(global-flycheck-mode +1)
 (add-hook 'after-init-hook 'global-company-mode)
 (add-hook 'after-init-hook 'envrc-global-mode)
