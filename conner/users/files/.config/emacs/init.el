90bae7e19324b9d3cdd50c05e49ce8c3030a326f
Conner Ebbinghaus <connerebbinghaus@gmail.com>
2024-12-20 15:49:03 -0500

    Init.el changes

--- conner/users/files/.config/emacs/init.el
+++ conner/users/files/.config/emacs/init.el
@@ -1,3 +1,12 @@
+;;; init.el --- Initialization file for Emacs
+;;; Commentary:
+;;; Emacs Startup File --- initialization for Emacs
+;;; Code:
+(require 'lsp-mode)
+(require 'flycheck)
+(require 'doom-modeline)
+(require 'geiser)
+
 (cua-mode t)
 (with-eval-after-load 'geiser-guile
   (add-to-list 'geiser-guile-load-path "~/Projects/guix")
@@ -5,11 +14,15 @@
   (add-to-list 'geiser-guile-load-path "~/Projects/sops-guix/modules")
   (add-to-list 'geiser-guile-load-path "~/Projects/guix-surface"))
 
-(require 'lsp-mode)
-(add-hook 'prog-mode-hook #'lsp-deferred)
+(add-hook 'rust-mode-hook #'lsp-deferred)
+(add-hook 'cpp-mode-hook #'lsp-deferred)
 
-(global-flycheck-mode +1)
+(add-hook 'after-init-hook #'global-flycheck-mode)
 (add-hook 'after-init-hook 'global-company-mode)
 (add-hook 'after-init-hook 'envrc-global-mode)
 (add-hook 'after-init-hook #'doom-modeline-mode)
 (add-hook 'after-init-hook (lambda () (load-theme 'tango-dark)))
+
+;; Fake the footer to avoid warnings
+;; (provide 'init)
+;;; init.el ends here
