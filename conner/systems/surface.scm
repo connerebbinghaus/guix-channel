3436a87505541aaaf5d9e70609eb255416a2e15a
Conner Ebbinghaus <connerebbinghaus@gmail.com>
2026-08-14 14:22:45 -0400

    Add surface

---
+++ conner/systems/surface.scm
@@ -0,0 +1,30 @@
+(define-module (conner systems surface)
+  #:use-module (conner systems desktop)
+  #:use-module (gnu system)
+  #:use-module (gnu system file-systems)
+  #:export (conner-surface-os))
+  
+  
+
+(define-public conner-surface-os
+  (operating-system
+   (inherit base-os-desktop)
+   (host-name "conner-surface")
+   (kernel-arguments
+    (cons*
+     "resume=/dev/nvme0n1p2"
+     "cfg80211.ieee80211_regdom=US"
+     (operating-system-user-kernel-arguments base-os-desktop)))
+   (swap-devices (list (swap-space
+			(target "/dev/nvme0n1p2")
+			(priority 0))))
+   (file-systems (cons* (file-system
+                         (mount-point "/boot/efi")
+                         (device (uuid "F282-91EA"
+                                       'fat32))
+                         (type "vfat"))
+			(file-system
+                         (mount-point "/")
+                         (device (uuid "e8a11dfb-0c64-4c24-b27b-2716a69337d4"))
+                         (type "btrfs")) 
+                        %base-file-systems))))
